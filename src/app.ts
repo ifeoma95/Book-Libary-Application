aa66d5d8d5513d50ad3d1dac72cab8ef5666717b
ifeoma95 <135955961+ifeoma95@users.noreply.github.com>
2023-09-03 22:20:22 +0100

    new update

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,11 @@ import mongoose from 'mongoose'
 import path from 'path'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+
+
 config();
+
+
 
 mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true })
 .then(()=>{
@@ -15,9 +19,13 @@ mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true })
   console.error(err)
 })
 
+
+
 import indexRouter from './routes/index'
 import usersRouter from './routes/author'
 import bookRouter from './routes/book'
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/dist/esm/standalone';
 
 const app = express();
 
